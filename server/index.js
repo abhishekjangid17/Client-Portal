776cd2cffb2a6087dc71aa63e262676cd4dd5b41
abhishekjangid1 <abjangid222@gmail.com>
2026-04-05 09:49:24 +0530

    Fix CORS for production

--- server/index.js
+++ server/index.js
@@ -4,22 +4,15 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const corsOptions = {
-  origin: [
-    'http://localhost:5173',
-    'https://client-portal-inky-three.vercel.app/'
-  ],
-  credentials: true,
-};
 
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+app.options('*', cors());
 app.use(express.json());
-
-const authRoutes = require('./routes/authRoutes');
-app.use('/api/auth', authRoutes);
-
-const projectRoutes = require('./routes/projectRoutes');
-app.use('/api/projects', projectRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
@@ -29,6 +22,12 @@ mongoose.connect(process.env.MONGO_URI)
   });
 
 app.get('/', (req, res) => res.send('Server is running'));
+
+const authRoutes = require('./routes/authRoutes');
+const projectRoutes = require('./routes/projectRoutes');
+
+app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
