8c9823c1b7638ceb9089b00f72d9485fbbc67737
abhishekjangid1 <abjangid222@gmail.com>
2026-04-05 09:55:51 +0530

    Fix Express 5 wildcard CORS options route

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-app.options('*', cors());
+app.options('/{*path}', cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
